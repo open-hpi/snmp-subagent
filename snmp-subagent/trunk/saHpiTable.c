af18cf16c172a886c563397b448c0568ef61c1d1
konradrzeszutek <konradrzeszutek@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2003-11-24 16:55:26 +0000

    Testing the SaHpiTimeT.

--- snmp-subagent/trunk/saHpiTable.c
+++ snmp-subagent/trunk/saHpiTable.c
@@ -310,11 +310,24 @@ saHpiTable_modify_context(SaHpiRptEntryT *entry,
 
     // IBM-KR: TODO , test this code. Make sure the time is right.
     memcpy(&ctx->saHpiEventLogTime, time, sizeof(SaHpiTimeT));
-    DEBUGMSGTL((AGENT,"TODO: Time is %d, or %d [%d]\n", time, ctx->saHpiEventLogTime.high,
+    DEBUGMSGTL((AGENT,"TODO: Time is %d, or high%d[%X];low:%d[%X]\n", 
+		time, 
+		ctx->saHpiEventLogTime.high,
+		ctx->saHpiEventLogTime.high,
+		ctx->saHpiEventLogTime.low,
 		ctx->saHpiEventLogTime.low));
     ctx->saHpiEventLogTime.high = htonl(ctx->saHpiEventLogTime.high);
     ctx->saHpiEventLogTime.low = htonl(ctx->saHpiEventLogTime.low);
    
+    DEBUGMSGTL((AGENT,"TODO: Time is %d, or %d [%X]; low: %d[%X]\n", time, 
+		ctx->saHpiEventLogTime.high,
+		ctx->saHpiEventLogTime.high,
+		ctx->saHpiEventLogTime.low,
+		ctx->saHpiEventLogTime.low));
+
+    ctx->saHpiEventLogTime.high = ntohl(ctx->saHpiEventLogTime.high);
+    ctx->saHpiEventLogTime.low = ntohl(ctx->saHpiEventLogTime.low);
+
     DEBUGMSGTL((AGENT,"TODO: Time is %d, or %d [%d]\n", time, ctx->saHpiEventLogTime.high,
 		ctx->saHpiEventLogTime.low));
     ctx->saHpiEventLogState = (*state == SAHPI_TRUE) ? MIB_TRUE: MIB_FALSE;
