a1b557a2d1a444eae5a39b17ffa11fb8a437e304
konradrzeszutek <konradrzeszutek@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-02-08 17:07:11 +0000

    Added a shared env and script function file. Finished first test-case

--- src/t/t0.sh
+++ src/t/t0.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
- # (C) Copyright IBM Corp. 2003
- #
- # This program is distributed in the hope that it will be useful,
- # but WITHOUT ANY WARRANTY; without even the implied warranty of
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
- # file and program are licensed under a BSD style license.  See
- # the Copying file included with the OpenHPI distribution for
- # full licensing terms.
- #
- # Authors:
- #   Konrad Rzeszutek <konradr@us.ibm.com>
- #
- # $Id$
- #
-exit 0
+# (C) Copyright IBM Corp. 2003
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+# file and program are licensed under a BSD style license.  See
+# the Copying file included with the OpenHPI distribution for
+# full licensing terms.
+#
+# Authors:
+#   Konrad Rzeszutek <konradr@us.ibm.com>
+#
+# $Id$ 
+#
+
+. ./shared.env
+
+if [ "$1" == "-v" ]; then
+	VERBOSE=yes
+fi
+
+start
+
+q $SNMPWALK -v2c -c $SNMPD_COMMUNITY $SNMPD_SOCKET hpi | grep -v Counter64 > $0.result
+
+check
+stop
