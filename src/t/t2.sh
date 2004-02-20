0efb37d1fff7e823a1ec4ba07268a2eb5336fabe
konradrzeszutek <konradrzeszutek@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-02-20 20:35:47 +0000

    Test-cases for bug #877063 (consolodate ..StateCategoryXXXX fields),

---
+++ src/t/t2.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# (C) Copyright IBM Corp. 2003
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+# file and program are licensed under a BSD style license.  See
+# the Copying file included with the OpenHPI distribution for
+# full licensing terms.
+#
+# Authors:
+#   Konrad Rzeszutek <konradr@us.ibm.com>
+#
+# $Id$ 
+#
+
+. ./shared.env
+
+if [ "$1" == "-v" ]; then
+	VERBOSE=yes
+fi
+
+start
+
+dbg "Set SensorAssertions"
+
+q $SNMPSET -v2c -c $SNMPD_COMMUNITY $SNMPD_SOCKET HPI-MIB::saHpiSensorAssertEvents.0.1.1 = UPPER_CRIT > $0.tmp
+q $SNMPGET -v2c -c $SNMPD_COMMUNITY $SNMPD_SOCKET HPI-MIB::saHpiSensorAssertEvents.0.1.1 >> $0.tmp
+q $SNMPSET -v2c -c $SNMPD_COMMUNITY $SNMPD_SOCKET HPI-MIB::saHpiSensorAssertEvents.0.1.1 = UPPER_CRIT,LOWER_MAJOR >> $0.tmp
+q $SNMPGET -v2c -c $SNMPD_COMMUNITY $SNMPD_SOCKET HPI-MIB::saHpiSensorAssertEvents.0.1.1 >> $0.tmp
+cat $0.tmp | grep -v $SNMPSET | grep -v $SNMPGET > $0.result
+rm $0.tmp
+
+check
+stop
