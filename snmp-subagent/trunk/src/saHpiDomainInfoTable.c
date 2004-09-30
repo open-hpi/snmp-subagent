53036feb6aaf48b41ddb1819fe2703572d3a83d6
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-09-30 19:51:53 +0000

    ArtifactID: Comment:Corrected index value.

--- snmp-subagent/trunk/src/saHpiDomainInfoTable.c
+++ snmp-subagent/trunk/src/saHpiDomainInfoTable.c
@@ -1140,7 +1140,7 @@ int populate_domain_info( void ) {
 	} else {
 
 	/* create domain tuple/ index */
-	domain_info_oids[1] = DomainInfo.DomainId; 
+	domain_info_oids[0] = DomainInfo.DomainId; 
 	domain_info_index.len =  DOMAIN_INFO_INDEX_LEN;
 	domain_info_index.oids = (oid *) & domain_info_oids;  
 
