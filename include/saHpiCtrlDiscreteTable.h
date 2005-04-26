0099e49abfcb090dbaea951ce92594ab8319fd65
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-04-26 18:45:42 +0000

    ArtifactID: Comment: Implemented scalar and populate calls.

--- include/saHpiCtrlDiscreteTable.h
+++ include/saHpiCtrlDiscreteTable.h
@@ -121,6 +121,14 @@ extern oid saHpiCtrlDiscreteTable_oid[];
 extern size_t saHpiCtrlDiscreteTable_oid_len;
 
 #define saHpiCtrlDiscreteTable_TABLE_OID 1,3,6,1,4,1,18568,2,1,1,4,7,4
+
+
+/* Number of table Indexes */
+#define CTRL_DISCRETE_INDEX_NR 4 
+#define saHpiDomainId_INDEX 0
+#define saHpiResourceEntryId_INDEX 1
+#define saHpiResourceIsHistorical_INDEX 2
+#define saHpiCtrlDiscreteEntryId_INDEX 3
     
 /*************************************************************
  * column number definitions for table saHpiCtrlDiscreteTable
