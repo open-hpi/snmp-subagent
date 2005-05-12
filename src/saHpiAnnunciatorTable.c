5b029b025a2b5b09b976d1aaa1572436c09ff393
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-05-12 20:07:26 +0000

    ArtifactID: Comment:Implemented _cmp().

--- src/saHpiAnnunciatorTable.c
+++ src/saHpiAnnunciatorTable.c
@@ -45,7 +45,6 @@ oid saHpiAnnunciatorTable_oid[] = { saHpiAnnunciatorTable_TABLE_OID };
 size_t saHpiAnnunciatorTable_oid_len = OID_LENGTH(saHpiAnnunciatorTable_oid);
 
 
-#ifdef saHpiAnnunciatorTable_IDX2
 /************************************************************
  * keep binary tree to find context by name
  */
@@ -67,70 +66,55 @@ saHpiAnnunciatorTable_cmp( const void *lhs, const void *rhs )
      * check primary key, then secondary. Add your own code if
      * there are more than 2 indexes
      */
-    int rc;
+	DEBUGMSGTL ((AGENT, "saHpiSensorTable_cmp, called\n"));
 
-    /*
-     * TODO: implement compare. Remove this ifdef code and
-     * add your own code here.
-     */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR,
-             "saHpiAnnunciatorTable_compare not implemented! Container order undefined\n" );
-    return 0;
-#endif
-    
-    /*
-     * EXAMPLE (assuming you want to sort on a name):
-     *   
-     * rc = strcmp( context_l->xxName, context_r->xxName );
-     *
-     * if(rc)
-     *   return rc;
-     *
-     * TODO: fix secondary keys (or delete if there are none)
-     *
-     * if(context_l->yy < context_r->yy) 
-     *   return -1;
-     *
-     * return (context_l->yy == context_r->yy) ? 0 : 1;
-     */
+	/* check for NULL pointers */
+	if (lhs == NULL || rhs == NULL ) {
+		DEBUGMSGTL((AGENT,"saHpiSensorTable_cmp() NULL pointer ERROR\n" ));
+		return 0;
+	}
+	/* CHECK FIRST INDEX,  saHpiDomainId */
+	if ( context_l->index.oids[0] < context_r->index.oids[0])
+		return -1;
+
+	if ( context_l->index.oids[0] > context_r->index.oids[0])
+		return 1;
+
+	if ( context_l->index.oids[0] == context_r->index.oids[0]) {
+		/* If saHpiDomainId index is equal sort by second index */
+		/* CHECK SECOND INDEX,  saHpiResourceEntryId */
+		if ( context_l->index.oids[1] < context_r->index.oids[1])
+			return -1;
+
+		if ( context_l->index.oids[1] > context_r->index.oids[1])
+			return 1;
+
+		if ( context_l->index.oids[1] == context_r->index.oids[1]) {
+			/* If saHpiResourceEntryId index is equal sort by third index */
+			/* CHECK THIRD INDEX,  saHpiResourceIsHistorical */
+			if ( context_l->index.oids[2] < context_r->index.oids[2])
+				return -1;
+
+			if ( context_l->index.oids[2] > context_r->index.oids[2])
+				return 1;
+
+			if ( context_l->index.oids[2] == context_r->index.oids[2]) {
+				/* If saHpiResourceIsHistorical index is equal sort by forth index */
+				/* CHECK FORTH INDEX,  saHpiAnnunciatorNum */
+				if ( context_l->index.oids[3] < context_r->index.oids[3])
+					return -1;
+
+				if ( context_l->index.oids[3] > context_r->index.oids[3])
+					return 1;
+
+				if ( context_l->index.oids[3] == context_r->index.oids[3])
+					return 0;
+			}
+		}
+	}
+
+	return 0;
 }
-
-/************************************************************
- * search tree
- */
-/** TODO: set additional indexes as parameters */
-saHpiAnnunciatorTable_context *
-saHpiAnnunciatorTable_get( const char *name, int len )
-{
-    saHpiAnnunciatorTable_context tmp;
-
-    /** we should have a secondary index */
-    netsnmp_assert(cb.container->next != NULL);
-    
-    /*
-     * TODO: implement compare. Remove this ifdef code and
-     * add your own code here.
-     */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR, "saHpiAnnunciatorTable_get not implemented!\n" );
-    return NULL;
-#endif
-
-    /*
-     * EXAMPLE:
-     *
-     * if(len > sizeof(tmp.xxName))
-     *   return NULL;
-     *
-     * strncpy( tmp.xxName, name, sizeof(tmp.xxName) );
-     * tmp.xxName_len = len;
-     *
-     * return CONTAINER_FIND(cb.container->next, &tmp);
-     */
-}
-#endif
-
 
 /************************************************************
  * Initializes the saHpiAnnunciatorTable module
@@ -140,13 +124,6 @@ init_saHpiAnnunciatorTable(void)
 {
     initialize_table_saHpiAnnunciatorTable();
 
-    /*
-     * TODO: perform any startup stuff here, such as
-     * populating the table with initial data.
-     *
-     * saHpiAnnunciatorTable_context * new_row = create_row(index);
-     * CONTAINER_INSERT(cb.container,new_row);
-     */
 }
 
 /************************************************************
@@ -193,8 +170,6 @@ static int saHpiAnnunciatorTable_row_copy(saHpiAnnunciatorTable_context * dst,
     return 0;
 }
 
-#ifdef saHpiAnnunciatorTable_SET_HANDLING
-
 /**
  * the *_extract_index routine
  *
@@ -240,42 +215,22 @@ saHpiAnnunciatorTable_extract_index( saHpiAnnunciatorTable_context * ctx, netsnm
        memset( &var_saHpiDomainId, 0x00, sizeof(var_saHpiDomainId) );
        var_saHpiDomainId.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
        /** TODO: link this index to the next, or NULL for the last one */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR, "saHpiAnnunciatorTable_extract_index index list not implemented!\n" );
-    return 0;
-#else
        var_saHpiDomainId.next_variable = &var_XX;
-#endif
 
        memset( &var_saHpiResourceId, 0x00, sizeof(var_saHpiResourceId) );
        var_saHpiResourceId.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
        /** TODO: link this index to the next, or NULL for the last one */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR, "saHpiAnnunciatorTable_extract_index index list not implemented!\n" );
-    return 0;
-#else
        var_saHpiResourceId.next_variable = &var_XX;
-#endif
 
        memset( &var_saHpiResourceIsHistorical, 0x00, sizeof(var_saHpiResourceIsHistorical) );
        var_saHpiResourceIsHistorical.type = ASN_INTEGER; /* type hint for parse_oid_indexes */
        /** TODO: link this index to the next, or NULL for the last one */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR, "saHpiAnnunciatorTable_extract_index index list not implemented!\n" );
-    return 0;
-#else
        var_saHpiResourceIsHistorical.next_variable = &var_XX;
-#endif
 
        memset( &var_saHpiAnnunciatorNum, 0x00, sizeof(var_saHpiAnnunciatorNum) );
        var_saHpiAnnunciatorNum.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
        /** TODO: link this index to the next, or NULL for the last one */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR, "saHpiAnnunciatorTable_extract_index index list not implemented!\n" );
-    return 0;
-#else
        var_saHpiAnnunciatorNum.next_variable = &var_XX;
-#endif
 
 
     /*
@@ -401,7 +356,6 @@ int saHpiAnnunciatorTable_can_delete(saHpiAnnunciatorTable_context *undo_ctx,
     return 1;
 }
 
-#ifdef saHpiAnnunciatorTable_ROW_CREATION
 /************************************************************
  * the *_create_row routine is called by the table handler
  * to create a new row for a given index. If you need more
@@ -448,7 +402,6 @@ saHpiAnnunciatorTable_create_row( netsnmp_index* hdr)
 
     return ctx;
 }
-#endif
 
 /************************************************************
  * the *_duplicate row routine
@@ -647,23 +600,6 @@ void saHpiAnnunciatorTable_set_action( netsnmp_request_group *rg )
         }
     }
 
-    /*
-     * done with all the columns. Could check row related
-     * requirements here.
-     */
-#ifndef saHpiAnnunciatorTable_CAN_MODIFY_ACTIVE_ROW
-    if( undo_ctx && RS_IS_ACTIVE(undo_ctx->saHpiDomainAlarmRowStatus) &&
-        row_ctx && RS_IS_ACTIVE(row_ctx->saHpiDomainAlarmRowStatus) ) {
-            row_err = 1;
-    }
-#endif
-
-    /*
-     * check activation/deactivation
-     */
-    row_err = netsnmp_table_array_check_row_status(&cb, rg,
-                                  row_ctx ? &row_ctx->saHpiDomainAlarmRowStatus : NULL,
-                                  undo_ctx ? &undo_ctx->saHpiDomainAlarmRowStatus : NULL);
     if(row_err) {
         netsnmp_set_mode_request_error(MODE_SET_BEGIN,
                                        (netsnmp_request_info*)rg->rg_void,
@@ -813,8 +749,6 @@ void saHpiAnnunciatorTable_set_undo( netsnmp_request_group *rg )
      */
 }
 
-#endif /** saHpiAnnunciatorTable_SET_HANDLING */
-
 
 /************************************************************
  *
@@ -879,18 +813,18 @@ initialize_table_saHpiAnnunciatorTable(void)
     cb.container = netsnmp_container_find("saHpiAnnunciatorTable_primary:"
                                           "saHpiAnnunciatorTable:"
                                           "table_container");
-#ifdef saHpiAnnunciatorTable_IDX2
+
     netsnmp_container_add_index(cb.container,
                                 netsnmp_container_find("saHpiAnnunciatorTable_secondary:"
                                                        "saHpiAnnunciatorTable:"
                                                        "table_container"));
     cb.container->next->compare = saHpiAnnunciatorTable_cmp;
-#endif
-#ifdef saHpiAnnunciatorTable_SET_HANDLING
+
+
     cb.can_set = 1;
-#ifdef saHpiAnnunciatorTable_ROW_CREATION
+
     cb.create_row = (UserRowMethod*)saHpiAnnunciatorTable_create_row;
-#endif
+
     cb.duplicate_row = (UserRowMethod*)saHpiAnnunciatorTable_duplicate_row;
     cb.delete_row = (UserRowMethod*)saHpiAnnunciatorTable_delete_row;
     cb.row_copy = (Netsnmp_User_Row_Operation *)saHpiAnnunciatorTable_row_copy;
@@ -905,7 +839,7 @@ initialize_table_saHpiAnnunciatorTable(void)
     cb.set_commit = saHpiAnnunciatorTable_set_commit;
     cb.set_free = saHpiAnnunciatorTable_set_free;
     cb.set_undo = saHpiAnnunciatorTable_set_undo;
-#endif
+
     DEBUGMSGTL(("initialize_table_saHpiAnnunciatorTable",
                 "Registering table saHpiAnnunciatorTable "
                 "as a table array\n"));
