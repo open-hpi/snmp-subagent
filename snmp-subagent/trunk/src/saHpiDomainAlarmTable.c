256ae256b8acb3071a00adc346de15d178c4b9ea
ddearauj <ddearauj@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-06-09 16:29:57 +0000

    ArtifactID: Comment: Implemented extract and compare functions.

--- snmp-subagent/trunk/src/saHpiDomainAlarmTable.c
+++ snmp-subagent/trunk/src/saHpiDomainAlarmTable.c
@@ -49,7 +49,6 @@ void populate_saHpiDomainAlarmTable()
 	dbg("WARNING: populate_saHpiDomainAlarmTable: not implemented!");
 }
 
-#ifdef saHpiDomainAlarmTable_IDX2
 /************************************************************
  * keep binary tree to find context by name
  */
@@ -62,42 +61,53 @@ static int saHpiDomainAlarmTable_cmp( const void *lhs, const void *rhs );
 static int
 saHpiDomainAlarmTable_cmp( const void *lhs, const void *rhs )
 {
-    saHpiDomainAlarmTable_context *context_l =
+        saHpiDomainAlarmTable_context *context_l =
         (saHpiDomainAlarmTable_context *)lhs;
-    saHpiDomainAlarmTable_context *context_r =
+        saHpiDomainAlarmTable_context *context_r =
         (saHpiDomainAlarmTable_context *)rhs;
 
-    /*
-     * check primary key, then secondary. Add your own code if
-     * there are more than 2 indexes
-     */
-    int rc;
+        /*
+         * check primary key, then secondary. Add your own code if
+         * there are more than 2 indexes
+         */
+        DEBUGMSGTL ((AGENT, "saHpiDomainAlarmTable_cmp, called\n"));
 
-    /*
-     * TODO: implement compare. Remove this ifdef code and
-     * add your own code here.
-     */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR,
-             "saHpiDomainAlarmTable_compare not implemented! Container order undefined\n" );
-    return 0;
-#endif
-    
-    /*
-     * EXAMPLE (assuming you want to sort on a name):
-     *   
-     * rc = strcmp( context_l->xxName, context_r->xxName );
-     *
-     * if(rc)
-     *   return rc;
-     *
-     * TODO: fix secondary keys (or delete if there are none)
-     *
-     * if(context_l->yy < context_r->yy) 
-     *   return -1;
-     *
-     * return (context_l->yy == context_r->yy) ? 0 : 1;
-     */
+        /* check for NULL pointers */
+        if (lhs == NULL || rhs == NULL ) {
+                DEBUGMSGTL((AGENT,"saHpiDomainAlarmTable_cmp() NULL pointer ERROR\n" ));
+                return 0;
+        }
+        /* CHECK FIRST INDEX,  saHpiDomainId */
+        if ( context_l->index.oids[0] < context_r->index.oids[0])
+                return -1;
+
+        if ( context_l->index.oids[0] > context_r->index.oids[0])
+                return 1;
+
+        if ( context_l->index.oids[0] == context_r->index.oids[0]) {
+                /* If saHpiDomainId index is equal sort by second index */
+                /* CHECK SECOND INDEX,  saHpiDoaminAlaramId */
+                if ( context_l->index.oids[1] < context_r->index.oids[1])
+                        return -1;
+
+                if ( context_l->index.oids[1] > context_r->index.oids[1])
+                        return 1;
+
+                if ( context_l->index.oids[1] == context_r->index.oids[1]) {
+                        /* If saHpiResourceEntryId index is equal sort by third index */
+                        /* CHECK THIRD INDEX,  saHpiDomainAlarmSeverity */
+                        if ( context_l->index.oids[2] < context_r->index.oids[2])
+                                return -1;
+
+                        if ( context_l->index.oids[2] > context_r->index.oids[2])
+                                return 1;
+
+                        if ( context_l->index.oids[2] == context_r->index.oids[2])
+                                return 0;
+                }
+        }
+
+        return 0;
 }
 
 /************************************************************
@@ -214,7 +224,6 @@ static int saHpiDomainAlarmTable_row_copy(saHpiDomainAlarmTable_context * dst,
     return 0;
 }
 
-#ifdef saHpiDomainAlarmTable_SET_HANDLING
 
 /**
  * the *_extract_index routine
@@ -227,111 +236,80 @@ static int saHpiDomainAlarmTable_row_copy(saHpiDomainAlarmTable_context * dst,
 int
 saHpiDomainAlarmTable_extract_index( saHpiDomainAlarmTable_context * ctx, netsnmp_index * hdr )
 {
-    /*
-     * temporary local storage for extracting oid index
-     *
-     * extract index uses varbinds (netsnmp_variable_list) to parse
-     * the index OID into the individual components for each index part.
-     */
-    /** TODO: add storage for external index(s)! */
-    netsnmp_variable_list var_saHpiDomainId;
-    netsnmp_variable_list var_saHpiDomainAlarmId;
-    netsnmp_variable_list var_saHpiDomainAlarmSeverity;
-    int err;
+        /*
+         * temporary local storage for extracting oid index
+         *
+         * extract index uses varbinds (netsnmp_variable_list) to parse
+         * the index OID into the individual components for each index part.
+         */
+        /** TODO: add storage for external index(s)! */
+        netsnmp_variable_list var_saHpiDomainId;
+        netsnmp_variable_list var_saHpiDomainAlarmId;
+        netsnmp_variable_list var_saHpiDomainAlarmSeverity;
+        int err;
 
-    /*
-     * copy index, if provided
-     */
-    if(hdr) {
-        netsnmp_assert(ctx->index.oids == NULL);
-        if(snmp_clone_mem( (void*)&ctx->index.oids, hdr->oids,
-                           hdr->len * sizeof(oid) )) {
-            return -1;
+        DEBUGMSGTL ((AGENT, "saHpiDomainAlarmTable_extract_index, called\n"));
+
+        /*
+         * copy index, if provided
+         */
+        if (hdr) {
+                netsnmp_assert(ctx->index.oids == NULL);
+                if (snmp_clone_mem( (void*)&ctx->index.oids, hdr->oids,
+                                    hdr->len * sizeof(oid) )) {
+                        return -1;
+                }
+                ctx->index.len = hdr->len;
         }
-        ctx->index.len = hdr->len;
-    }
 
-    /*
-     * initialize variable that will hold each component of the index.
-     * If there are multiple indexes for the table, the variable_lists
-     * need to be linked together, in order.
-     */
-       /** TODO: add code for external index(s)! */
-       memset( &var_saHpiDomainId, 0x00, sizeof(var_saHpiDomainId) );
-       var_saHpiDomainId.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
-       /** TODO: link this index to the next, or NULL for the last one */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR, "saHpiDomainAlarmTable_extract_index index list not implemented!\n" );
-    return 0;
-#else
-       var_saHpiDomainId.next_variable = &var_XX;
-#endif
+        /*
+         * initialize variable that will hold each component of the index.
+         * If there are multiple indexes for the table, the variable_lists
+         * need to be linked together, in order.
+         */
+        /** TODO: add code for external index(s)! */
+        memset( &var_saHpiDomainId, 0x00, sizeof(var_saHpiDomainId) );
+        var_saHpiDomainId.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
+        /** TODO: link this index to the next, or NULL for the last one */
+        var_saHpiDomainId.next_variable = &var_saHpiDomainAlarmId;
 
-       memset( &var_saHpiDomainAlarmId, 0x00, sizeof(var_saHpiDomainAlarmId) );
-       var_saHpiDomainAlarmId.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
-       /** TODO: link this index to the next, or NULL for the last one */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR, "saHpiDomainAlarmTable_extract_index index list not implemented!\n" );
-    return 0;
-#else
-       var_saHpiDomainAlarmId.next_variable = &var_XX;
-#endif
+        memset( &var_saHpiDomainAlarmId, 0x00, sizeof(var_saHpiDomainAlarmId) );
+        var_saHpiDomainAlarmId.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
+        /** TODO: link this index to the next, or NULL for the last one */
+        var_saHpiDomainAlarmId.next_variable = &var_saHpiDomainAlarmSeverity;
 
-       memset( &var_saHpiDomainAlarmSeverity, 0x00, sizeof(var_saHpiDomainAlarmSeverity) );
-       var_saHpiDomainAlarmSeverity.type = ASN_INTEGER; /* type hint for parse_oid_indexes */
-       /** TODO: link this index to the next, or NULL for the last one */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR, "saHpiDomainAlarmTable_extract_index index list not implemented!\n" );
-    return 0;
-#else
-       var_saHpiDomainAlarmSeverity.next_variable = &var_XX;
-#endif
+        memset( &var_saHpiDomainAlarmSeverity, 0x00, sizeof(var_saHpiDomainAlarmSeverity) );
+        var_saHpiDomainAlarmSeverity.type = ASN_INTEGER; /* type hint for parse_oid_indexes */
+        /** TODO: link this index to the next, or NULL for the last one */
+        var_saHpiDomainAlarmSeverity.next_variable = NULL;
 
 
-    /*
-     * parse the oid into the individual index components
-     */
-    err = parse_oid_indexes( hdr->oids, hdr->len, &var_saHpiDomainId );
-    if (err == SNMP_ERR_NOERROR) {
-       /*
-        * copy index components into the context structure
-        */
-              /** skipping external index saHpiDomainId */
+
+        /*
+         * parse the oid into the individual index components
+         */
+        err = parse_oid_indexes( hdr->oids, hdr->len, &var_saHpiDomainId );
+        if (err == SNMP_ERR_NOERROR) {
+                /*
+                 * copy index components into the context structure
+                 */
+                /** skipping external index saHpiDomainId */
    
                 ctx->saHpiDomainAlarmId = *var_saHpiDomainAlarmId.val.integer;
    
                 ctx->saHpiDomainAlarmSeverity = *var_saHpiDomainAlarmSeverity.val.integer;
    
-   
-           /*
-            * TODO: check index for valid values. For EXAMPLE:
-            *
-              * if ( *var_saHpiDomainId.val.integer != XXX ) {
-          *    err = -1;
-          * }
-          */
-           /*
-            * TODO: check index for valid values. For EXAMPLE:
-            *
-              * if ( *var_saHpiDomainAlarmId.val.integer != XXX ) {
-          *    err = -1;
-          * }
-          */
-           /*
-            * TODO: check index for valid values. For EXAMPLE:
-            *
-              * if ( *var_saHpiDomainAlarmSeverity.val.integer != XXX ) {
-          *    err = -1;
-          * }
-          */
-    }
+                err = saHpiDomainId_check_index(*var_saHpiDomainId.val.integer); 
+		err = saHpiDomainAlarmId_check_index(*var_saHpiDomainAlarmId.val.integer);
+		err = saHpiDomainAlarmSeverity_check_index(*var_saHpiDomainAlarmSeverity.val.integer);
+        }
 
-    /*
-     * parsing may have allocated memory. free it.
-     */
-    snmp_reset_var_buffers( &var_saHpiDomainId );
+        /*
+         * parsing may have allocated memory. free it.
+         */
+        snmp_reset_var_buffers( &var_saHpiDomainId );
 
-    return err;
+        return err;
 }
 
 /************************************************************
