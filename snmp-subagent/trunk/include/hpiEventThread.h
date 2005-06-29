f0b4f64b52dd9603a1495980739af759c5df7bdf
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-06-29 21:01:52 +0000

    ArtifactID: Comment:Added event handling for UserEvents.

--- snmp-subagent/trunk/include/hpiEventThread.h
+++ snmp-subagent/trunk/include/hpiEventThread.h
@@ -18,9 +18,11 @@
 #ifndef _HPI_EVENT_THREAD_
 #define _HPI_EVENT_THREAD_
 
-int start_event_thread(void);
+int start_event_thread(SaHpiSessionIdT sessionid);
 gboolean get_run_threaded(void);
 void set_run_threaded(gboolean val);
+
+extern GMutex *thread_mutex;
 
 #endif /*_HPI_EVENT_THREAD_*/
 
