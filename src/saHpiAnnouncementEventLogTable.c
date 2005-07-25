d428ed2feb4a5ccd67816e7b915ce65acc18c646
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-07-25 21:04:25 +0000

    ArtifactID: Comment:Removed DomainAlarmId

--- src/saHpiAnnouncementEventLogTable.c
+++ src/saHpiAnnouncementEventLogTable.c
@@ -132,7 +132,6 @@ int initialize_table_saHpiAnnouncementEventLogEntryCount(void)
 /************************************************************/
 
 
-#ifdef saHpiAnnouncementEventLogTable_IDX2
 /************************************************************
  * keep binary tree to find context by name
  */
@@ -179,7 +178,7 @@ saHpiAnnouncementEventLogTable_cmp( const void *lhs, const void *rhs )
 
                 if ( context_l->index.oids[1] == context_r->index.oids[1]) {
                         /* If saHpiResourceEntryId index is equal sort by third index */
-                        /* CHECK THIRD INDEX,  saHpiDomainAlarmId */
+                        /* CHECK THIRD INDEX,  saHpiAnnouncementEntryId */
                         if ( context_l->index.oids[2] < context_r->index.oids[2])
                                 return -1;
 
@@ -187,58 +186,13 @@ saHpiAnnouncementEventLogTable_cmp( const void *lhs, const void *rhs )
                                 return 1;
 
                         if ( context_l->index.oids[2] == context_r->index.oids[2]) {
-                                /* If saHpiDomainAlarmId index is equal sort by forth index */
-                                /* CHECK FORTH INDEX,  saHpiAnnouncementEntryId */
-                                if ( context_l->index.oids[3] < context_r->index.oids[3])
-                                        return -1;
-
-                                if ( context_l->index.oids[3] > context_r->index.oids[3])
-                                        return 1;
-
-                                if ( context_l->index.oids[3] == context_r->index.oids[3])
-                                        return 0;
+                                return 0;
                         }
                 }
         }
 
         return 0;
 }
-
-/************************************************************
- * search tree
- */
-/** TODO: set additional indexes as parameters */
-saHpiAnnouncementEventLogTable_context *
-saHpiAnnouncementEventLogTable_get( const char *name, int len )
-{
-    saHpiAnnouncementEventLogTable_context tmp;
-
-    /** we should have a secondary index */
-    netsnmp_assert(cb.container->next != NULL);
-    
-    /*
-     * TODO: implement compare. Remove this ifdef code and
-     * add your own code here.
-     */
-#ifdef TABLE_CONTAINER_TODO
-    snmp_log(LOG_ERR, "saHpiAnnouncementEventLogTable_get not implemented!\n" );
-    return NULL;
-#endif
-
-    /*
-     * EXAMPLE:
-     *
-     * if(len > sizeof(tmp.xxName))
-     *   return NULL;
-     *
-     * strncpy( tmp.xxName, name, sizeof(tmp.xxName) );
-     * tmp.xxName_len = len;
-     *
-     * return CONTAINER_FIND(cb.container->next, &tmp);
-     */
-}
-#endif
-
 
 /************************************************************
  * Initializes the saHpiAnnouncementEventLogTable module
@@ -338,7 +292,6 @@ saHpiAnnouncementEventLogTable_extract_index( saHpiAnnouncementEventLogTable_con
         /** TODO: add storage for external index(s)! */
         netsnmp_variable_list var_saHpiDomainId;
         netsnmp_variable_list var_saHpiResourceId;
-        netsnmp_variable_list var_saHpiDomainAlarmId;
         netsnmp_variable_list var_saHpiAnnouncementEntryId;
         int err;
 
@@ -370,12 +323,7 @@ saHpiAnnouncementEventLogTable_extract_index( saHpiAnnouncementEventLogTable_con
         memset( &var_saHpiResourceId, 0x00, sizeof(var_saHpiResourceId) );
         var_saHpiResourceId.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
         /** TODO: link this index to the next, or NULL for the last one */
-        var_saHpiResourceId.next_variable = &var_saHpiDomainAlarmId;
-
-        memset( &var_saHpiDomainAlarmId, 0x00, sizeof(var_saHpiDomainAlarmId) );
-        var_saHpiDomainAlarmId.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
-        /** TODO: link this index to the next, or NULL for the last one */
-        var_saHpiDomainAlarmId.next_variable = &var_saHpiAnnouncementEntryId;
+        var_saHpiResourceId.next_variable = &var_saHpiAnnouncementEntryId;
 
         memset( &var_saHpiAnnouncementEntryId, 0x00, sizeof(var_saHpiAnnouncementEntryId) );
         var_saHpiAnnouncementEntryId.type = ASN_UNSIGNED; /* type hint for parse_oid_indexes */
@@ -395,13 +343,10 @@ saHpiAnnouncementEventLogTable_extract_index( saHpiAnnouncementEventLogTable_con
    
                 /** skipping external index saHpiResourceId */
    
-                /** skipping external index saHpiDomainAlarmId */
-   
                 /** skipping external index saHpiAnnouncementEntryId */
    
                 err = saHpiDomainId_check_index(*var_saHpiDomainId.val.integer);
                 err = saHpiResourceEntryId_check_index(*var_saHpiResourceId.val.integer);  
-                err = saHpiDomainAlarmId_check_index(*var_saHpiDomainAlarmId.val.integer);
 		err = saHpiAnnouncementEntryId_check_index(*var_saHpiAnnouncementEntryId.val.integer);   
 
         }
@@ -482,7 +427,6 @@ int saHpiAnnouncementEventLogTable_can_delete(saHpiAnnouncementEventLogTable_con
     return 1;
 }
 
-#ifdef saHpiAnnouncementEventLogTable_ROW_CREATION
 /************************************************************
  * the *_create_row routine is called by the table handler
  * to create a new row for a given index. If you need more
@@ -540,7 +484,6 @@ saHpiAnnouncementEventLogTable_create_row( netsnmp_index* hdr)
 
     return ctx;
 }
-#endif
 
 /************************************************************
  * the *_duplicate row routine
@@ -1009,23 +952,6 @@ void saHpiAnnouncementEventLogTable_set_action( netsnmp_request_group *rg )
         }
     }
 
-    /*
-     * done with all the columns. Could check row related
-     * requirements here.
-     */
-#ifndef saHpiAnnouncementEventLogTable_CAN_MODIFY_ACTIVE_ROW
-    if( undo_ctx && RS_IS_ACTIVE(undo_ctx->saHpiDomainAlarmRowStatus) &&
-        row_ctx && RS_IS_ACTIVE(row_ctx->saHpiDomainAlarmRowStatus) ) {
-            row_err = 1;
-    }
-#endif
-
-    /*
-     * check activation/deactivation
-     */
-    row_err = netsnmp_table_array_check_row_status(&cb, rg,
-                                  row_ctx ? &row_ctx->saHpiDomainAlarmRowStatus : NULL,
-                                  undo_ctx ? &undo_ctx->saHpiDomainAlarmRowStatus : NULL);
     if(row_err) {
         netsnmp_set_mode_request_error(MODE_SET_BEGIN,
                                        (netsnmp_request_info*)rg->rg_void,
@@ -1307,8 +1233,6 @@ void saHpiAnnouncementEventLogTable_set_undo( netsnmp_request_group *rg )
      */
 }
 
-#endif /** saHpiAnnouncementEventLogTable_SET_HANDLING */
-
 
 /************************************************************
  *
@@ -1358,8 +1282,6 @@ initialize_table_saHpiAnnouncementEventLogTable(void)
         netsnmp_table_helper_add_index(table_info, ASN_UNSIGNED);
         /** index: saHpiResourceId */
         netsnmp_table_helper_add_index(table_info, ASN_UNSIGNED);
-        /** index: saHpiDomainAlarmId */
-        netsnmp_table_helper_add_index(table_info, ASN_UNSIGNED);
         /** index: saHpiAnnouncementEntryId */
         netsnmp_table_helper_add_index(table_info, ASN_UNSIGNED);
 
@@ -1373,18 +1295,18 @@ initialize_table_saHpiAnnouncementEventLogTable(void)
     cb.container = netsnmp_container_find("saHpiAnnouncementEventLogTable_primary:"
                                           "saHpiAnnouncementEventLogTable:"
                                           "table_container");
-#ifdef saHpiAnnouncementEventLogTable_IDX2
+
     netsnmp_container_add_index(cb.container,
                                 netsnmp_container_find("saHpiAnnouncementEventLogTable_secondary:"
                                                        "saHpiAnnouncementEventLogTable:"
                                                        "table_container"));
     cb.container->next->compare = saHpiAnnouncementEventLogTable_cmp;
-#endif
-#ifdef saHpiAnnouncementEventLogTable_SET_HANDLING
+
+
     cb.can_set = 1;
-#ifdef saHpiAnnouncementEventLogTable_ROW_CREATION
+
     cb.create_row = (UserRowMethod*)saHpiAnnouncementEventLogTable_create_row;
-#endif
+
     cb.duplicate_row = (UserRowMethod*)saHpiAnnouncementEventLogTable_duplicate_row;
     cb.delete_row = (UserRowMethod*)saHpiAnnouncementEventLogTable_delete_row;
     cb.row_copy = (Netsnmp_User_Row_Operation *)saHpiAnnouncementEventLogTable_row_copy;
@@ -1399,7 +1321,7 @@ initialize_table_saHpiAnnouncementEventLogTable(void)
     cb.set_commit = saHpiAnnouncementEventLogTable_set_commit;
     cb.set_free = saHpiAnnouncementEventLogTable_set_free;
     cb.set_undo = saHpiAnnouncementEventLogTable_set_undo;
-#endif
+
     DEBUGMSGTL(("initialize_table_saHpiAnnouncementEventLogTable",
                 "Registering table saHpiAnnouncementEventLogTable "
                 "as a table array\n"));
