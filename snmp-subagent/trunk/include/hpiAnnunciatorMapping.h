5b91ecd0fa4e5627b8c59d454520ee53b1620b94
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-08-17 13:56:42 +0000

    ArtifactID: Comment:

--- snmp-subagent/trunk/include/hpiAnnunciatorMapping.h
+++ snmp-subagent/trunk/include/hpiAnnunciatorMapping.h
@@ -15,3 +15,23 @@
  *					  
  */
 
+#ifndef _HPI_ANNUNCIATOR_MAPPING_
+#define _HPI_ANNUNCIATOR_MAPPING_
+
+#define MAPPING_FILE "hpiAnnunciatorMapping"
+typedef struct {
+	SaHpiEntryIdT key_tuple_array[3];
+} key_tuple;
+
+
+typedef struct {
+        SaHpiEntryIdT   hpi_entry_id;
+        key_tuple       dre_pair;
+} DRE_XREF;
+
+/* hpi internal apis */
+SaErrorT domain_resource_entry_initialize(int *initialized, GHashTable **hash_table); 
+DRE_XREF *domain_resource_entry_get(key_tuple *ep, GHashTable **hash_table); 
+DRE_XREF *domain_resoruce_entry_lookup(key_tuple *ep, GHashTable **hash_table);
+
+#endif /* _HPI_ANNUNCIATOR_MAPPING_ */
