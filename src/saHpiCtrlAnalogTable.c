94fbcd7a3e4a2557527551a2c3fa8e06553673c8
ddearauj <ddearauj@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-09-16 20:21:12 +0000

    ArtifactID: Comment: Change deprecatd calls from netsnmp_request_set_mode_error to netsnmp_request_set_error.

--- src/saHpiCtrlAnalogTable.c
+++ src/saHpiCtrlAnalogTable.c
@@ -919,7 +919,7 @@ void saHpiCtrlAnalogTable_set_reserve1( netsnmp_request_group *rg )
 		}
 
 		if (rc)
-			netsnmp_set_mode_request_error(MODE_SET_BEGIN, current->ri, rc );
+			netsnmp_request_set_error( current->ri, rc );
 		rg->status = SNMP_MAX( rg->status, current->ri->status );
 	}
 
@@ -979,7 +979,7 @@ void saHpiCtrlAnalogTable_set_reserve2( netsnmp_request_group *rg )
 		}
 
 		if (rc)
-			netsnmp_set_mode_request_error(MODE_SET_BEGIN, current->ri, rc);
+			netsnmp_request_set_error( current->ri, rc);
 	}
 
 	/*
@@ -1038,8 +1038,7 @@ void saHpiCtrlAnalogTable_set_action( netsnmp_request_group *rg )
 	}
 
 	if (row_err) {
-		netsnmp_set_mode_request_error(MODE_SET_BEGIN,
-					       (netsnmp_request_info*)rg->rg_void,
+		netsnmp_request_set_error((netsnmp_request_info*)rg->rg_void,
 					       row_err);
 		return;
 	}
--- src/saHpiEventLogTable.c
+++ src/saHpiEventLogTable.c
@@ -1094,7 +1094,7 @@ void saHpiEventLogTable_set_reserve1( netsnmp_request_group *rg )
         }
 
         if (rc)
-           netsnmp_set_mode_request_error(MODE_SET_BEGIN, current->ri, rc );
+           netsnmp_request_set_error( current->ri, rc );
         rg->status = SNMP_MAX( rg->status, current->ri->status );
     }
 
@@ -1130,7 +1130,7 @@ void saHpiEventLogTable_set_reserve2( netsnmp_request_group *rg )
         }
 
         if (rc)
-           netsnmp_set_mode_request_error(MODE_SET_BEGIN, current->ri, rc);
+           netsnmp_request_set_error( current->ri, rc);
     }
 
     /*
@@ -1195,8 +1195,7 @@ void saHpiEventLogTable_set_action( netsnmp_request_group *rg )
 #endif
 
     if(row_err) {
-        netsnmp_set_mode_request_error(MODE_SET_BEGIN,
-                                       (netsnmp_request_info*)rg->rg_void,
+        netsnmp_request_set_error((netsnmp_request_info*)rg->rg_void,
                                        row_err);
         return;
     }
