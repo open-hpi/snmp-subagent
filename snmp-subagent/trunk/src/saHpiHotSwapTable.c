823438872e01f6b1cb081d70407b81e28074f2ea
ddearauj <ddearauj@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-09-16 20:17:25 +0000

    ArtifactID: Comment: Changed deprecrated calls from netsnmp_request_set_mode_error to netsnmp_request_set_error.

--- snmp-subagent/trunk/src/saHpiHotSwapTable.c
+++ snmp-subagent/trunk/src/saHpiHotSwapTable.c
@@ -910,7 +910,7 @@ void saHpiHotSwapTable_set_reserve1( netsnmp_request_group *rg )
         }
 
         if (rc)
-           netsnmp_set_mode_request_error(MODE_SET_BEGIN, current->ri, rc );
+           netsnmp_request_set_error( current->ri, rc );
         rg->status = SNMP_MAX( rg->status, current->ri->status );
     }
 
@@ -1019,7 +1019,7 @@ void saHpiHotSwapTable_set_reserve2( netsnmp_request_group *rg )
         }
 
         if (rc)
-           netsnmp_set_mode_request_error(MODE_SET_BEGIN, current->ri, rc);
+           netsnmp_request_set_error( current->ri, rc);
     }
 
     /*
@@ -1111,8 +1111,7 @@ void saHpiHotSwapTable_set_action( netsnmp_request_group *rg )
      * check activation/deactivation
      */
     if(row_err) {
-        netsnmp_set_mode_request_error(MODE_SET_BEGIN,
-                                       (netsnmp_request_info*)rg->rg_void,
+        netsnmp_request_set_error((netsnmp_request_info*)rg->rg_void,
                                        row_err);
         return;
     }
--- snmp-subagent/trunk/src/saHpiOEMEventTable.c
+++ snmp-subagent/trunk/src/saHpiOEMEventTable.c
@@ -882,7 +882,7 @@ void saHpiOEMEventTable_set_reserve1( netsnmp_request_group *rg )
         }
 
         if (rc)
-           netsnmp_set_mode_request_error(MODE_SET_BEGIN, current->ri, rc );
+           netsnmp_request_set_error( current->ri, rc );
         rg->status = SNMP_MAX( rg->status, current->ri->status );
     }
 
@@ -918,7 +918,7 @@ void saHpiOEMEventTable_set_reserve2( netsnmp_request_group *rg )
         }
 
         if (rc)
-           netsnmp_set_mode_request_error(MODE_SET_BEGIN, current->ri, rc);
+           netsnmp_request_set_error( current->ri, rc);
     }
 
     /*
@@ -967,8 +967,7 @@ void saHpiOEMEventTable_set_action( netsnmp_request_group *rg )
      * requirements here.
      */
     if(row_err) {
-        netsnmp_set_mode_request_error(MODE_SET_BEGIN,
-                                       (netsnmp_request_info*)rg->rg_void,
+        netsnmp_request_set_error((netsnmp_request_info*)rg->rg_void,
                                        row_err);
         return;
     }
