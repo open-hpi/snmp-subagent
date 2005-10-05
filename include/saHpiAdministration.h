546d8dd327924b45568a39da0ed8b747441bcb56
ddearauj <ddearauj@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-10-05 15:39:28 +0000

    ArtifactID: Comment: Initial creation for administration scalars.

---
+++ include/saHpiAdministration.h
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright IBM Corp. 2005
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *   David Judkovics  <djudkovi@us.ibm.com>
+ *   Daniel de Araujo <ddearauj@us.ibm.com>
+ *
+ * (C) Copyright IBM Corp. 2005
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *   Daniel de Araujo  <ddearauj@us.ibm.com>
+ *
+ *					  
+ */
+
+#ifndef SAHPIADMINISTRATION_H
+#define SAHPIADMINISTRATION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/library/container.h>
+#include <net-snmp/agent/table_array.h>
+
+
+/*************************************************************
+ * function declarations
+ */
+void init_saHpiAdministration(void);
+
+#endif
