87914c9674f9ed5597a8fe71bd110d184561884f
ddearauj <ddearauj@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-10-05 17:39:39 +0000

    ArtifactID: Comment: Now repopulates all tables when saHpiDiscover is set with 1.

--- snmp-subagent/trunk/include/saHpiAdministration.h
+++ snmp-subagent/trunk/include/saHpiAdministration.h
@@ -11,19 +11,6 @@
  * Authors:
  *   David Judkovics  <djudkovi@us.ibm.com>
  *   Daniel de Araujo <ddearauj@us.ibm.com>
- *
- * (C) Copyright IBM Corp. 2005
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
- * file and program are licensed under a BSD style license.  See
- * the Copying file included with the OpenHPI distribution for
- * full licensing terms.
- *
- * Authors:
- *   Daniel de Araujo  <ddearauj@us.ibm.com>
- *
  *					  
  */
 
@@ -44,5 +31,7 @@ extern "C" {
  * function declarations
  */
 void init_saHpiAdministration(void);
+
+void repopulate_tables(SaHpiSessionIdT session_id);
 
 #endif
